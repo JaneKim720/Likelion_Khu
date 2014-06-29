46ef3b0726c49acb62eb83c7f119b9a2ae498eea
서 현덕 <SeoHyeonDeok@seoui-MacBook-Pro.local>
2014-06-29 21:35:35 +0900

    added simple pusher chatting with ajax

--- SeoLikelion/app/views.py
+++ SeoLikelion/app/views.py
@@ -1,5 +1,11 @@
 from flask import render_template, Flask, jsonify, request
 from app import app
+import sys
+sys.path.append("/Users/SeoHyeonDeok/studying/github/LikelionKhu_using_git/Likelion_Khu/SeoLikelion/pusher")
+import pusher
+
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -11,5 +17,13 @@ def index():
 def add_numbers():
 
     a = request.args.get('a')
+
+
+    p = pusher.Pusher(
+        app_id='79161',
+        key='00c655f076d9940088e8',
+        secret='c4e74cc8e2a00cd553fb'
+    )
+    p['seokhulikelion'].trigger('my_event', {'message': a})
 
     return jsonify(result=a)
