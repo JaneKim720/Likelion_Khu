41b2f1cbbe10b736ceab3347db78ae566ce1e765
서 현덕 <SeoHyeonDeok@seoui-MacBook-Pro.local>
2014-06-29 20:45:35 +0900

    Adding ajax term to give chatting statement to server Asynchronously

--- SeoLikelion/app/views.py
+++ SeoLikelion/app/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, Flask, jsonify, request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
+
     return render_template('index.html')
+
+@app.route('/_add_numbers')
+def add_numbers():
+
+    a = request.args.get('a')
+
+    return jsonify(result=a)
